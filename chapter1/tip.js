961ef58b20cab9e24fa6cff950a4bd5db817d8c0
Kristen Cabrera <40896596+kris10cabrera@users.noreply.github.com>
2022-02-12 17:57:11 -0500

    `send()` that accepts amount in input, converts to wei + to HEX to send

--- chapter1/tip.js
+++ chapter1/tip.js
@@ -1,4 +1,29 @@
+const web3 = new Web3(Web3.givenProvider);
 const form = document.querySelector("form");
+const input = form.querySelector("input");
+
+const send = async (amount) => {
+  const accounts = await window.ethereum.request({
+    method: "eth_requestAccounts",
+  });
+
+  // eth value's base unit is wei. 10^18 wei = 1 ETH
+  const wei = web3.utils.toWei(amount, "ether");
+  const hexxxdWei = web3.utils.toHex(wei);
+
+  if (accounts.length > 0) {
+    window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [
+        {
+          from: accounts[0],
+          to: "0x8A05fA58d533a6e40C4381E3247Cf4c68ca61cdc",
+          value: hexxxdWei,
+        },
+      ],
+    });
+  }
+};
 
 if (window.ethereum) {
   form.classList.add("has-eth");
@@ -7,7 +32,9 @@ if (window.ethereum) {
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  if (!window.ethereum) {
+  if (window.ethereum) {
+    send(input.value);
+  } else {
     alert("Please install a wallet.");
   }
 });
